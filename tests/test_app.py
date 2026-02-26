a5f6969a33b681389167d067a30c8098c7934b19
Ms. Jerush G <urk23cs1131@KARUNYA.EDU>
2026-02-26 15:23:19 +0530

    Updated app.py with new logic

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from app import add
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
-    assert add(0, 0) == 0  # Added a "zero case" for better coverage
