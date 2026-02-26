a3e8c61d2caa96fabd0be0a2ad9277efdfbabf4f
Ms. Jerush G <urk23cs1131@KARUNYA.EDU>
2026-02-26 15:00:04 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b  # Indented correctly
+
+if __name__ == "__main__":
+    print("Hello from Python CI Lab!")
+    # Combined the string onto one line for clarity
+    print("2 + 3 =", add(2, 3))
